1d6b4ead5c5aa14ea0ef5614bd4a2ca2c94245e7
Mohamed elhaddioui <114026809+medmedhaddioui@users.noreply.github.com>
2025-02-12 19:22:20 +0000

    Update

--- CPP-Module-08/ex02/MutantStack.hpp
+++ CPP-Module-08/ex02/MutantStack.hpp
@@ -9,9 +9,28 @@ class MutantStack
     private:
         std::vector<T> vec;
     public:
-        MutantStack();
-        ~MutantStack();
-}
+        MutantStack()
+        {
+            std::cout<< "Constructor Called !" << std::endl;
+        }
+        ~MutantStack()
+        {
+            std::cout << "Destructor Called !" << std::endl;
+        }
+        void push (const T value)
+        {
+            vec.push_back(value);
+        }
+        size_t size()
+        {
+            return vec.size();
+        }
+        void printTmp()
+        {
+            for (typename std::vector <T>::iterator it = vec.begin (); it != vec.end() ; it ++)
+                std::cout  << *it << std::endl;
+        }
+};
 
 
 #endif
--- CPP-Module-08/ex02/main.cpp
+++ CPP-Module-08/ex02/main.cpp
@@ -1,5 +1,9 @@
-#include ""
+#include "MutantStack.hpp"
 int main ()
 {
-
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    mstack.push(1222);
+    mstack.printTmp();
 }
