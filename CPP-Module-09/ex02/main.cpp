3ec8d60a84f8a941748edee701aaf6ee6e61dd51
Mohammed El Haddioui <mel-hadd@c3r9p10.1337.ma>
2025-02-28 23:58:46 +0000

    ed

--- CPP-Module-09/ex02/main.cpp
+++ CPP-Module-09/ex02/main.cpp
@@ -1,20 +1,32 @@
 #include "PmergeMe.hpp"
 
-
-
-int main (int ac , char **av)
+// int main (int ac , char **av)
+// {
+//     if (ac == 1)
+//         return 1;
+//     try
+//     {
+//         PmergeMe p;
+//         for (int i = 1; i < ac ; i++)
+//             p.checkValidArgs(av[i]);
+//         p.MergeInsertAlgorithm();
+//     }
+//     catch(const std::exception& e)
+//     {
+//         std::cerr << e.what() << std::endl;
+//     }
+// }
+void MergeSort(std::vector <int> &vec, int left , int right)
 {
-    if (ac == 1)
-        return 1;
-    try
+    if (left < right)
     {
-        PmergeMe p;
-        for (int i = 1; i < ac ; i++)
-            p.checkValidArgs(av[i]);
-        p.MergeInsertAlgorithm();
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
+         int mid = left + (right - left) / 2;
     }
 }
+int main ()
+{
+    std::vector <int> vec ({9,3,7,5,6,4,8,2});
+    int n = vec.size();
+    MergeSort(vec, 0 , n - 1);
+}
+
